1723268dfb95861407d205cdcc8b83a4524a1a18
ruhibhandariqspl <ruhi.bhandari@quagnitia.com>
2018-03-05 12:04:07 +0530

    Tecaher module changes

--- app/src/main/java/com/quagnitia/studentattendance/admin/AddTeacherActivity.java
+++ app/src/main/java/com/quagnitia/studentattendance/admin/AddTeacherActivity.java
@@ -116,6 +116,7 @@ public class AddTeacherActivity extends AppCompatActivity implements View.OnClic
         hashMap.put("Fullname",edt_full_name.getText().toString());
         hashMap.put("EmailId",edt_emailid.getText().toString());
         hashMap.put("MobileNo",edt_mobileno.getText().toString());
+        hashMap.put("Classname",sp_class.getSelectedItem().toString());
         hashMap.put("Username",edt_username.getText().toString());
         hashMap.put("Password",edt_password.getText().toString());
         new WebService(this,this,hashMap,"editTeacher").execute(AppConstants.BASE_URL+AppConstants.EDIT_TEACHER);
